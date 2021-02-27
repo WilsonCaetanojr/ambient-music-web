423ce7ab9c53a6fe3df781ec155d2212ec5e5433
Wilson Caetano <wilsonjr_caetano@hotmail.com>
2021-02-27 16:42:26 -0300

    Modifica estilização e estrutura da navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -6,12 +6,10 @@ import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
-import AccountCircle from "@material-ui/icons/AccountCircle";
 import MenuItem from "@material-ui/core/MenuItem";
 import Menu from "@material-ui/core/Menu";
 import Avatar from "@material-ui/core/Avatar";
 import "./navbar.css";
-import zIndex from "@material-ui/core/styles/zIndex";
 
 const useStyles = makeStyles(theme => ({
   menuButton: {
@@ -25,12 +23,9 @@ const useStyles = makeStyles(theme => ({
 export default function MenuAppBar() {
   const classes = useStyles();
   const [sidebar, setSidebar] = useContext(SidebarContext);
-  const [auth, setAuth] = React.useState(true);
-  const [openAvatar, setOpenAvatar] = React.useState(false);
   const [anchorEl, setAnchorEl] = React.useState(null);
-  //   const open = Boolean(anchorEl);
 
-  const handleMenu = event => {
+  const handleClick = event => {
     setAnchorEl(event.currentTarget);
   };
 
@@ -40,10 +35,7 @@ export default function MenuAppBar() {
 
   return (
     <div className="navRoot">
-      <AppBar
-        position="static"
-        style={{ backgroundColor: "#202024", height: 70 }}
-      >
+      <AppBar position="static" className="app-bar">
         <Toolbar>
           <IconButton
             edge="start"
@@ -54,56 +46,42 @@ export default function MenuAppBar() {
           >
             <MenuIcon className="menuIcon" />
           </IconButton>
-          <img
-            src="./logoNav.png"
-            style={{ width: "50%", maxWidth: 300 }}
-          ></img>
+          <img src="./logoNav.png" className="logo-nav"></img>
           <Typography variant="h6" className={classes.title}></Typography>
-          {auth && (
-            <div>
-              <IconButton
-                aria-label="account of current user"
-                aria-controls="menu-appbar"
+
+          <div>
+            <IconButton
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleClick}
+              color="inherit"
+            >
+              <Avatar
+                aria-controls="simple-menu"
                 aria-haspopup="true"
-                onClick={() => setOpenAvatar(!openAvatar)}
-                color="inherit"
+                className="avatar"
               >
-                <Avatar
-                  className="avatar"
-                  style={{
-                    backgroundColor: "#5b4699",
-                    width: 45,
-                    height: 45,
-                    border: "solid 2px white"
-                  }}
-                >
-                  <label className="labelAvatar">
-                    {"user" && "user.Nome"
-                      ? "Wilson".substring(0, 1).toUpperCase()
-                      : "A"}
-                  </label>
-                </Avatar>
-              </IconButton>
-              <Menu
-                id="menu-appbar"
-                anchorEl={anchorEl}
-                anchorOrigin={{
-                  vertical: "top",
-                  horizontal: "right"
-                }}
-                keepMounted
-                transformOrigin={{
-                  vertical: "top",
-                  horizontal: "right"
-                }}
-                open={openAvatar}
-                onClose={handleClose}
-              >
-                <MenuItem onClick={handleClose}>Minha conta</MenuItem>
-                <MenuItem onClick={handleClose}>Sair</MenuItem>
-              </Menu>
-            </div>
-          )}
+                <label className="labelAvatar">
+                  {"user" && "user.Nome"
+                    ? "Wilson".substring(0, 1).toUpperCase()
+                    : "A"}
+                </label>
+              </Avatar>
+            </IconButton>
+
+            <Menu
+              id="simple-menu"
+              className="nav-menu-avartar"
+              anchorEl={anchorEl}
+              keepMounted
+              open={Boolean(anchorEl)}
+              onClose={handleClose}
+            >
+              <MenuItem onClick={handleClose}>Minha conta</MenuItem>
+              <MenuItem onClick={handleClose}>Sair</MenuItem>
+            </Menu>
+          </div>
         </Toolbar>
       </AppBar>
     </div>
