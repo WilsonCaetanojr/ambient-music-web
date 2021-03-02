b411b5dda36d427bb1efd867f236a852ea0856d8
Wilson Caetano <wilsonjr_caetano@hotmail.com>
2021-03-02 18:24:39 -0300

    Cria css global

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Routes from "./routes";
 import SidebarContext from "./context/sidebarContext";
+import "./styles/global.css";
 
 function App() {
   const [sidebar, setSidebar] = useState({ open: false });
