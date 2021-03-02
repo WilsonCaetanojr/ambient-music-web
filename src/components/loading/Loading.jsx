6d2bf331949d0e0dacb3d633edbafdea81e82721
Wilson Caetano <wilsonjr_caetano@hotmail.com>
2021-03-02 20:32:52 -0300

    Cria componente de loading

---
+++ src/components/loading/Loading.jsx
@@ -0,0 +1,24 @@
+import Backdrop from "@material-ui/core/Backdrop";
+import { makeStyles } from "@material-ui/core/styles";
+import "./loading.css";
+
+const useStyles = makeStyles(theme => ({
+  backdrop: {
+    zIndex: theme.zIndex.drawer + 1,
+    color: "#fff"
+  }
+}));
+
+const Loading = ({ loading = false }) => {
+  const classes = useStyles();
+
+  return (
+    <Backdrop className={classes.backdrop} open={loading}>
+      <div className="container-loading">
+        <img src="./loading.gif" />
+      </div>
+    </Backdrop>
+  );
+};
+
+export default Loading;
