45f743488142ffe5609f72be5a27eff17fb64d97
Wilson Caetano <wilsonjr_caetano@hotmail.com>
2021-03-02 21:46:45 -0300

    Add loading

--- src/pages/homePage/HomePage.jsx
+++ src/pages/homePage/HomePage.jsx
@@ -1,19 +1,28 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Navbar from "../../components/navbar/Navbar";
 import Sidebar from "../../components/sidebar/Sidebar";
 import CardMusic from "../../components/cardMusic/CardMusic";
+import Loading from "../../components/loading/Loading";
 import "./homePage.css";
 
 const HomePage = () => {
   const [albums, setAlbums] = useState([
     { title: "Epic", description: "Example music album" }
   ]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(function () {
+      setLoading(false);
+    }, 3000);
+  }, []);
 
   return (
     <>
       <Navbar />
       <Sidebar />
       <div className="container-home">
+        <Loading loading={loading} />
         <CardMusic albums={albums} />
       </div>
     </>
