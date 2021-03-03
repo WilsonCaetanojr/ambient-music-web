bfe53f1694bf06eb8a6adbfe8631d2ce19e7c84e
Wilson Caetano <wilsonjr_caetano@hotmail.com>
2021-03-03 20:57:49 -0300

    Aprimora tela de login

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,44 +1,47 @@
+import { useHistory } from "react-router-dom";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
 import "./login.css";
-import { makeStyles } from '@material-ui/core/styles';
-import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
-
-const useStyles = makeStyles((theme) => ({
-    root: {
-        '& > *': {
-            margin: theme.spacing(1),
-            width: '25ch',
-        },
-    },
-}));
 
 const Login = () => {
+  const { push } = useHistory();
 
-    const classes = useStyles();
-    
+  const handleSubmit = () => {
+    push("homePage");
+  };
 
-    return (
-        <div className="pai-div">
-            <div className="container-login">
-
-                <div className="container-login2">
-                    <img className="title-login" src="./images/fundooo.png" />
-                </div>
-
-                <div className="container-input">
-                    <TextField className="input-login" id="standard-basic" label="Login" />
-                    <TextField className="input-senha" type="password" id="standard-basic" label="Senha" />
-                </div>
-
-                <div className="button-login">
-                    <Button className="button-ajuste" variant="contained" color="primary">
-                        Entrar </Button>
-
-
-                </div>
-
-            </div>
+  return (
+    <div className="full-screen">
+      <div className="container-login">
+        <div className="container-img">
+          <img src="./images/logoLogin.png" />
         </div>
-    );
-}
+
+        <div className="container-input">
+          <TextField
+            className="input-login"
+            id="standard-basic"
+            label="Login"
+          />
+          <TextField
+            className="input-senha"
+            type="password"
+            id="standard-basic"
+            label="Senha"
+          />
+        </div>
+
+        <div className="container-button-login">
+          <Button
+            className="button-login"
+            variant="contained"
+            onClick={handleSubmit}
+          >
+            Entrar
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+};
 export default Login;
