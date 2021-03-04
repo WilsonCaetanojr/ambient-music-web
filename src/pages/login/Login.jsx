145ee1336b56a2f8e38b4229f9470c56c72947c7
Wilson Caetano <wilsonjr_caetano@hotmail.com>
2021-03-04 19:42:05 -0300

    Modifica botão da tela de login

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,10 +1,27 @@
 import { useHistory } from "react-router-dom";
 import TextField from "@material-ui/core/TextField";
+import { makeStyles } from "@material-ui/core/styles";
 import Button from "@material-ui/core/Button";
 import "./login.css";
 
+const useStyle = makeStyles(theme => ({
+  root: {
+    background: "linear-gradient(45deg, #ab6bfe 30%, #5a8bf6 90%)",
+    border: 0,
+    borderRadius: 3,
+    boxShadow: "0 3px 5px 2px rgba(255, 105, 135, .3)",
+    color: "white",
+    height: 48,
+    padding: "0 30px",
+    "&:hover": {
+      background: "linear-gradient(45deg, #8245d3 30%, #5a8bf6 90%)"
+    }
+  }
+}));
+
 const Login = () => {
   const { push } = useHistory();
+  const classes = useStyle();
 
   const handleSubmit = () => {
     push("homePage");
@@ -33,7 +50,7 @@ const Login = () => {
 
         <div className="container-button-login">
           <Button
-            className="button-login"
+            className={classes.root}
             variant="contained"
             onClick={handleSubmit}
           >
