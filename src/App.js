b8fa19961cbb924b5ab3e7af059f93ad289b3385
WilsonCaetanojr <wilsonjr_caetano@hotmail.com>
2021-03-08 20:24:20 -0300

    Cria context para o player

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import Routes from "./routes";
 import { ToastContainer } from "react-toastify";
+import PlayerContext from "./contex/Player";
 import "./styles/global.css";
+import { useState } from "react";
+import Player from "./components/player/Player";
 
 function App() {
+  const [player, setPlayer] = useState({ openModal: false });
+
   return (
-    <>
+    <PlayerContext.Provider value={[player, setPlayer]}>
       <Routes />
       <ToastContainer />
-    </>
+      <Player />
+    </PlayerContext.Provider>
   );
 }
 
