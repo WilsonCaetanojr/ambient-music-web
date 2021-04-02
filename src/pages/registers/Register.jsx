8874155eec90174bd261d63ab3c64793988d61e5
WilsonCaetanojr <wilsonjr_cartano@hotmail.com>
2021-04-02 13:27:52 -0300

    Altera nome do diretório para correção

--- src/pages/registers/Register.jsx
+++ src/pages/registers/Register.jsx
@@ -13,7 +13,7 @@ import IconButton from "@material-ui/core/IconButton";
 import InputAdornment from "@material-ui/core/InputAdornment";
 import isEmail from "../../utils/isEmail";
 
-const Login = () => {
+const Register = () => {
   const [password, setPassword] = useState("");
   const [confirmPassword, setconfirmPassword] = useState("");
   const [email, setEmail] = useState("");
@@ -111,4 +111,4 @@ const Login = () => {
     </div>
   );
 };
-export default Login;
+export default Register;
