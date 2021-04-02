f02cffd58f1622f305529dd2e144fbf1ca36b84b
WilsonCaetanojr <wilsonjr_cartano@hotmail.com>
2021-04-02 13:25:45 -0300

    Configura conexão com a api

---
+++ src/services/api.js
@@ -0,0 +1,109 @@
+import axios from "axios";
+import notify from "./../utils/notify";
+
+const defaultErrorMessages = {
+  503: "Não foi possível esabelecer uma conexão com o servidor. Verifique sua internet e tente novamente.",
+};
+
+axios.interceptors.response.use(
+  response => {
+    if (
+      response.status === 200 &&
+      response.data.warnings &&
+      Array.isArray(response.data.warnings)
+    ) {
+      response.data.warnings.map(e => notify(e, true, "warning"));
+    }
+
+    return response;
+  },
+  error => {
+    //Erro de conexão
+    if (error.message && error.message === "Network Error") {
+      notify(
+        "Não foi possível esabelecer uma conexão com o servidor. Verifique sua internet e tente novamente."
+      );
+      return Promise.reject(error);
+    }
+
+    if (
+      error.data &&
+      error.data.warnings &&
+      Array.isArray(error.data.warnings)
+    ) {
+      error.data.warnings.map(e => notify(e, true, "warning"));
+    }
+
+    if (
+      error.response &&
+      error.response.data &&
+      Array.isArray(error.response.data.errors)
+    ) {
+      for (const msg of error.response.data.errors) {
+        notify(msg, true);
+      }
+    }
+
+    if (
+      error.response &&
+      error.response.data &&
+      Array.isArray(error.response.data)
+    ) {
+      for (const msg of error.response.data) {
+        notify(msg, true);
+      }
+    }
+
+    const expectedError =
+      error.response &&
+      error.response.status >= 400 &&
+      error.response.status < 500;
+
+    //isCancel verifica se a requisição foi cancelada pelo usuário
+    if (!expectedError && !axios.isCancel(error)) {
+      console.log("Logging the error: " + error);
+      console.log(error.response);
+
+      if (
+        error.response.data &&
+        error.response.data.indexOf("File format not accepted") !== -1
+      ) {
+        notify(
+          "Respeite os formatos permitidos para o anexo: PNG, JPEG e JPG."
+        );
+      } else {
+        notify("Um erro inesperado aconteceu.");
+      }
+    } else if (expectedError) {
+      if (defaultErrorMessages[error.response.status]) {
+        notify(defaultErrorMessages[error.response.status], true);
+      } else if (
+        error.response &&
+        error.response.data &&
+        typeof error.response.data === "string"
+      ) {
+        notify(error.response.data, error.response.status !== 412);
+      }
+    }
+
+    return Promise.reject(error);
+  }
+);
+
+axios.defaults.baseURL = process.env.REACT_APP_URL_API;
+
+axios.defaults.timeout = 10000;
+
+function setHeaders(value = {}) {
+  axios.defaults.headers.common["token-ambient-music"] =
+    value.Token || sessionStorage.Token || null;
+}
+
+export default {
+  get: axios.get,
+  post: axios.post,
+  put: axios.put,
+  delete: axios.delete,
+  baseURL: axios.defaults.baseURL,
+  setHeaders,
+};
