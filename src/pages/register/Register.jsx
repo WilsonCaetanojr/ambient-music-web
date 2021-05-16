720d788b33040d9ab38773d7c64be55de2af8e51
WilsonCaetano-Qyon <wilsonjr_cartano@hotmail.com>
2021-05-16 12:02:34 -0300

    Finaliza tela de cadastro de usuário

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -12,8 +12,12 @@ import Input from "@material-ui/core/Input";
 import IconButton from "@material-ui/core/IconButton";
 import InputAdornment from "@material-ui/core/InputAdornment";
 import isEmail from "../../utils/isEmail";
+import notify from "../../utils/notify";
+import api from "../../services/api";
+import Loading from "../../components/loading/Loading";
 
 const Register = () => {
+  const [loading, setLoading] = useState(false);
   const [password, setPassword] = useState("");
   const [confirmPassword, setconfirmPassword] = useState("");
   const [email, setEmail] = useState("");
@@ -21,94 +25,121 @@ const Register = () => {
   const [showconfirmPassword, setShowconfirmPassword] = useState(false);
   const { push, goBack } = useHistory();
 
-  const handleSubmit = () => {
-    if (password !== confirmPassword) {
-      alert("As senhas estão diferentes");
-    } else {
-      if (!email || !isEmail(email)) {
-        alert("Email incorreto");
-      } else {
-        if (password.length < 5) {
-          alert("a senha é muito curta");
-        } else {
-          push("Login");
-        }
+  const handleSubmit = async e => {
+    try {
+      e.preventDefault();
+      setLoading(true);
+
+      let error = false;
+
+      if (password !== confirmPassword) {
+        error = true;
+        notify("As senhas estão diferentes.");
       }
+
+      if (!email || !isEmail(email)) {
+        error = true;
+        notify("Email inválido.");
+      }
+
+      if (password.length < 5) {
+        error = true;
+        notify("a senha é muito curta");
+      }
+
+      if (error) return setLoading(false);
+
+      const { data } = await api.post("/users", {
+        Email: email,
+        Password: password,
+        FlagActive: true,
+        Name: email.split("@")[0],
+      });
+
+      if (data.success) push("Login");
+    } catch (error) {
+      setLoading(false);
+      console.log("handleSubmit Register ERROR", error);
     }
   };
 
   return (
-    <div className="full-screen-register">
-      <div className="container-login-register">
-        <div className="container-header-register">
-          <ArrowBackIcon className="iconGoBack" onClick={goBack} />
-          <img src="./images/logoLogin.png" />
-        </div>
-        <div className="container-inputs-register">
-          <TextField
-            type="email"
-            required
-            value={email}
-            onChange={e => setEmail(e.target.value)}
-            className="input-email-register"
-            id="email"
-            label="Email"
-          />
-          <FormControl className="input-password-register">
-            <InputLabel htmlFor="standard-adornment-password">
-              Password
-            </InputLabel>
-            <Input
-              id="standard-adornment-password"
-              type={showPassword ? "text" : "password"}
-              value={password}
-              onChange={e => setPassword(e.target.value)}
-              endAdornment={
-                <InputAdornment position="end">
-                  <IconButton
-                    aria-label="toggle password visibility"
-                    onClick={() => setShowPassword(!showPassword)}
-                  >
-                    {showPassword ? <Visibility /> : <VisibilityOff />}
-                  </IconButton>
-                </InputAdornment>
-              }
+    <form onSubmit={handleSubmit}>
+      <Loading loading={loading} />
+      <div className="full-screen-register">
+        <div className="container-login-register">
+          <div className="container-header-register">
+            <ArrowBackIcon className="iconGoBack" onClick={goBack} />
+            <img src="./images/logoLogin.png" />
+          </div>
+          <div className="container-inputs-register">
+            <TextField
+              type="email"
+              required
+              value={email}
+              onChange={e => setEmail(e.target.value)}
+              className="input-email-register"
+              id="email"
+              label="Email"
             />
-          </FormControl>
-          <FormControl className="input-confirm-pass-register">
-            <InputLabel htmlFor="standard-adornment-confirmPassword">
-              Confirm Password
-            </InputLabel>
-            <Input
-              id="standard-adornment-confirmPassword"
-              type={showconfirmPassword ? "text" : "password"}
-              value={confirmPassword}
-              onChange={e => setconfirmPassword(e.target.value)}
-              endAdornment={
-                <InputAdornment position="end">
-                  <IconButton
-                    aria-label="toggle confirmPassword visibility"
-                    onClick={() => setShowconfirmPassword(!showconfirmPassword)}
-                  >
-                    {showconfirmPassword ? <Visibility /> : <VisibilityOff />}
-                  </IconButton>
-                </InputAdornment>
-              }
-            />
-          </FormControl>
-        </div>
+            <FormControl className="input-password-register">
+              <InputLabel htmlFor="standard-adornment-password">
+                Password
+              </InputLabel>
+              <Input
+                id="standard-adornment-password"
+                type={showPassword ? "text" : "password"}
+                value={password}
+                onChange={e => setPassword(e.target.value)}
+                endAdornment={
+                  <InputAdornment position="end">
+                    <IconButton
+                      aria-label="toggle password visibility"
+                      onClick={() => setShowPassword(!showPassword)}
+                    >
+                      {showPassword ? <Visibility /> : <VisibilityOff />}
+                    </IconButton>
+                  </InputAdornment>
+                }
+              />
+            </FormControl>
+            <FormControl className="input-confirm-pass-register">
+              <InputLabel htmlFor="standard-adornment-confirmPassword">
+                Confirm Password
+              </InputLabel>
+              <Input
+                id="standard-adornment-confirmPassword"
+                type={showconfirmPassword ? "text" : "password"}
+                value={confirmPassword}
+                onChange={e => setconfirmPassword(e.target.value)}
+                endAdornment={
+                  <InputAdornment position="end">
+                    <IconButton
+                      aria-label="toggle confirmPassword visibility"
+                      onClick={() =>
+                        setShowconfirmPassword(!showconfirmPassword)
+                      }
+                    >
+                      {showconfirmPassword ? <Visibility /> : <VisibilityOff />}
+                    </IconButton>
+                  </InputAdornment>
+                }
+              />
+            </FormControl>
+          </div>
 
-        <div className="container-button-login-register">
-          <Button
-            className="buton-gradient"
-            variant="contained"
-            onClick={handleSubmit}
-          >
-            Criar Conta
-          </Button>
+          <div className="container-button-login-register">
+            <Button
+              className="buton-gradient"
+              variant="contained"
+              type="subimit"
+            >
+              Criar Conta
+            </Button>
+          </div>
         </div>
       </div>
-    </div>
+    </form>
   );
 };
 export default Register;
